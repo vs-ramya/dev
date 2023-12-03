8c29db77168b7da8ca06b955073aaec0d43ba5ab
vs-ramya <sramya8051@gmail.com>
2023-12-02 22:52:01 -0800

    Added list item

--- dom.js
+++ dom.js
@@ -5,5 +5,9 @@ addedItems.style.fontWeight='bold';
 addedItems.style.color='green';
 let makeBold=document.getElementById("items");
 makeBold.style.fontWeight="bold";
-let item3=document.getElementsByClassName("list-group-item");
-item3[2].style.background="green";
+//let item3=document.getElementsByClassName("list-group-item");
+//item3[2].style.background="green";
+let item2=document.getElementsByTagName("li");
+for(let i=0;i<item2.length;i++) {
+item2[i].style.background="yellow";
+}
