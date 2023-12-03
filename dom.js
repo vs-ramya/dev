f9c44c26ba15955415f4b6a64992e68eddeaac30
vs-ramya <sramya8051@gmail.com>
2023-12-02 17:12:55 -0800

    manipulation

---
+++ dom.js
@@ -0,0 +1,5 @@
+let title=document.getElementById("header-title");
+title.style.border="2px solid black";
+let addedItems=document.getElementById("added");
+addedItems.style.fontWeight='bold';
+addedItems.style.color='green';
